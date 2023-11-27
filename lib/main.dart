c1162698f556df6074e28e0276112edeb4a577b4
Sadik Athanikar <athanikarsadik212@gmail.com>
2023-11-27 19:07:44 +0530

    Welcome screen and navbar added

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/pages/nav_pages/main_page.dart';
+import 'package:travel_app/pages/welcome_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            iconTheme: IconThemeData(
+              color:Colors.black
+            ),
+            elevation: 0,
+            backgroundColor: Colors.white
+          )
+        ),
+        home: MainPage());
+  }
+}
---
+++ lib/pages/nav_pages/main_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/pages/nav_pages/bar_item_page.dart';
+import 'package:travel_app/pages/nav_pages/home_page.dart';
+import 'package:travel_app/pages/nav_pages/my_page.dart';
+import 'package:travel_app/pages/nav_pages/search_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List pages = [const HomePage(), const BarItemPage(), const MyPage(), const SearchPage()];
+  int currentIndex = 0;
+  void onTap(int index){
+    setState(() {
+      currentIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        selectedItemColor: Colors.black54,
+        unselectedItemColor: Colors.grey.withOpacity(0.5),
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        onTap: onTap,
+        elevation: 0,
+        items: const [
+        BottomNavigationBarItem(label: "Home", icon: Icon(Icons.apps)),
+        BottomNavigationBarItem(
+            label: "Bar", icon: Icon(Icons.bar_chart_sharp)),
+        BottomNavigationBarItem(label: "Search", icon: Icon(Icons.search)),
+        BottomNavigationBarItem(label: "My", icon: Icon(Icons.person))
+      ]),
+    );
+  }
+}
